f5c85613ac76d37bfa3927866f8fc172f3720763
charleschaochen <399931011@qq.com>
2014-11-12 21:48:06 +0800

    Add helloworld code

---
+++ src/org/charlestech/jenkins/JenkinsTest.java
@@ -0,0 +1,10 @@
+package org.charlestech.jenkins;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey Jenkins!");
+	}
+
+}
